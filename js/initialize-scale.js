13e50e67c9bfc33e8385cfd87baa45bd2e6ef21b
endophasie <albina.tokarchuk@gmail.com>
2017-02-20 22:07:48 +0700

    Fix transform value

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,34 +1,36 @@
 'use strict';
 
-window.initializeScale = (function (container, step, defaultVal) {
-  var mainPhoto = document.querySelector('.filter-image-preview');
-  var dec = container.querySelector('.upload-resize-controls-button-dec');
-  var inc = container.querySelector('.upload-resize-controls-button-inc');
-  var resizeField = container.querySelector('.upload-resize-controls-value');
+window.initializeScale = (function () {
+  return function (container, step, defaultVal) {
+    var mainPhoto = document.querySelector('.filter-image-preview');
+    var dec = container.querySelector('.upload-resize-controls-button-dec');
+    var inc = container.querySelector('.upload-resize-controls-button-inc');
+    var resizeField = container.querySelector('.upload-resize-controls-value');
 
-  var changeVal = function () {
-    var min = 25;
-    var max = 100;
+    var changeVal = function () {
+      var min = 25;
+      var max = 100;
 
-    if (event.target === dec) {
-      defaultVal = defaultVal - step;
-      if (defaultVal < min) {
-        defaultVal = min;
+      if (event.target === dec) {
+        defaultVal = defaultVal - step;
+        if (defaultVal < min) {
+          defaultVal = min;
+        }
+
+      } else if (event.target === inc) {
+        defaultVal = defaultVal + step;
+
+        if (defaultVal > max) {
+          defaultVal = max;
+        }
       }
 
-    } else if (event.target === inc) {
-      defaultVal = defaultVal + step;
+      mainPhoto.style.transform = 'scale(' + defaultVal / 100 + ')';
 
-      if (defaultVal > max) {
-        defaultVal = max;
-      }
-    }
+      resizeField.value = defaultVal + '%';
+    };
 
-    mainPhoto.style.transform = 'scale(' + defaultVal / 100 + ')';
-
-    resizeField.value = defaultVal + '%';
-  };
-
-  dec.addEventListener('click', changeVal);
-  inc.addEventListener('click', changeVal);
-})(document.querySelector('.upload-resize-controls'), 25, 100);
+    dec.addEventListener('click', changeVal);
+    inc.addEventListener('click', changeVal);
+  }
+})();
