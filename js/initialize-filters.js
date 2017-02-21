c624d1211558ec4563c211e3d57208ac785734d7
endophasie <albina.tokarchuk@gmail.com>
2017-02-21 19:18:51 +0700

    Module 6 task 2

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,34 +1,17 @@
 'use strict';
 
-var applyFilter = function (name) {
-  var mainPhotoFilter = document.querySelector('.filter-image-preview');
-
-  name = name.replace('upload-', '');
-
-  mainPhotoFilter.classList = '';
-  mainPhotoFilter.classList.add('filter-image-preview', name);
-};
-
 window.initializeFilters = (function () {
-  var initializeFilters = function () {
+  var initializeFilters = function (setFilter) {
     var formControls = document.querySelector('.upload-filter-controls');
 
     var ENTER_KEY = 13;
 
-    var setMainPhotoFilter = function (name) {
-      var mainPhotoFilter = document.querySelector('.filter-image-preview');
-
-      name = name.replace('upload-', '');
-
-      mainPhotoFilter.classList = '';
-      mainPhotoFilter.classList.add('filter-image-preview', name);
-    };
-
     formControls.addEventListener('click', function (event) {
       if (event.target.getAttribute('name', 'upload-filter')) {
         var radioName = event.target.id;
+        radioName = radioName.replace('upload-', '');
 
-        setMainPhotoFilter(radioName);
+        setFilter(radioName);
       }
     }, true);
 
@@ -38,8 +21,9 @@ window.initializeFilters = (function () {
           var filterName = event.target.getAttribute('for');
           var radioFilter = formControls.querySelector('#' + filterName);
 
-          radioFilter.setAttribute('checked', true);
-          setMainPhotoFilter(filterName);
+          filterName = filterName.replace('upload-', '');
+          radioFilter.setAttribute('checked', 'checked');
+          setFilter(filterName);
       }
     }, true);
   };
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,38 +1,35 @@
 'use strict';
 
-var adjustScale = function (scale) {
-  var mainPhoto = document.querySelector('.filter-image-preview');
-
-  mainPhoto.style.transform = 'scale(' + scale / 100 + ')';
-};
-
 window.initializeScale = (function () {
-  return function (container, step, defaultVal) {
-    var mainPhoto = document.querySelector('.filter-image-preview');
+  return function (container, step, defaultVal, scaleFunction) {
     var dec = container.querySelector('.upload-resize-controls-button-dec');
     var inc = container.querySelector('.upload-resize-controls-button-inc');
     var resizeField = container.querySelector('.upload-resize-controls-value');
+    var currentVal;
+
+    scaleFunction(defaultVal);
+    resizeField.value = defaultVal + '%';
+    currentVal = defaultVal;
 
     var changeVal = function () {
       var min = 25;
       var max = 100;
 
       if (event.target === dec) {
-        defaultVal = defaultVal - step;
-        if (defaultVal < min) {
-          defaultVal = min;
+        currentVal = currentVal - step;
+        if (currentVal < min) {
+          currentVal = min;
         }
       } else if (event.target === inc) {
-        defaultVal = defaultVal + step;
+        currentVal = currentVal + step;
 
-        if (defaultVal > max) {
-          defaultVal = max;
+        if (currentVal > max) {
+          currentVal = max;
         }
       }
 
-      scaleFunction(defaultVal);
-
-      resizeField.value = defaultVal + '%';
+      scaleFunction(currentVal);
+      resizeField.value = currentVal + '%';
     };
 
     dec.addEventListener('click', changeVal);
