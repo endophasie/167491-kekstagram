4bb18aaf39e3eae3450ff5922d755a1fc5e159f8
endophasie <albina.tokarchuk@gmail.com>
2017-02-22 15:18:09 +0700

    Base

---
+++ js/pictures.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.pictures = (function () {
+  var picURL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
+
+  //window.load(picURL)
+
+  //window.showGallery()
+})();
---
+++ js/show-gallery.js
@@ -0,0 +1,5 @@
+'use strict';
+
+window.showGallery = (function () {
+
+})();
