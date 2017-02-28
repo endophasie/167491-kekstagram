3e3f6b65a9284fa9075377f47d9b990f185902cd
endophasie <albina.tokarchuk@gmail.com>
2017-02-28 14:59:31 +0700

    Update

--- js/pictures.js
+++ js/pictures.js
@@ -3,24 +3,25 @@
 window.pictures = (function () {
   var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/kekstagram/data';
   var pictures = [];
+  var tmplPicture = document.querySelector('#picture-template');
+  var contentPicture = tmplPicture.content.querySelector('.picture');
 
   var ENTER_KEY = 13;
 
   var onLoadPictures = function (data) {
     pictures = data;
     showFilters();
-    loadRenderPictures();
+    loadrenderPicture(pictures);
   };
 
-  var loadRenderPictures = function (arrayPic) {
+  var loadrenderPicture = function (arrayPic) {
     var blockPictures = document.querySelector('.pictures');
     var fragment = document.createDocumentFragment();
-    var arrayToRender = arrayPic || pictures;
 
     blockPictures.innerHTML = '';
 
-    arrayToRender.forEach(function (it) {
-      fragment.appendChild(renderPictures(it));
+    arrayPic.forEach(function (it) {
+      fragment.appendChild(renderPicture(it));
     });
 
     blockPictures.appendChild(fragment);
@@ -34,52 +35,47 @@ window.pictures = (function () {
     blockFilters.addEventListener('click', function (event) {
       switch (event.target.id) {
         case 'filter-popular' :
-          loadRenderPictures();
+          loadrenderPicture(pictures);
           break;
         case 'filter-new' :
           var newPictures = pictures.slice().sort(window.arrayUtils.shuffle).slice(0, 11);
 
-          loadRenderPictures(newPictures);
+          loadrenderPicture(newPictures);
           break;
         case 'filter-discussed' :
           var discussedPictures = pictures.slice().sort(window.arrayUtils.sortCommentsDec);
 
-          loadRenderPictures(discussedPictures);
+          loadrenderPicture(discussedPictures);
           break;
       }
     }, true);
   };
 
-  var renderPictures = (function () {
-    var tmplPicture = document.querySelector('#picture-template');
-    var contentPicture = tmplPicture.content.querySelector('.picture');
+  var renderPicture = function (picture) {
+    var elementPicture = contentPicture.cloneNode(true);
+    var pictureImg = elementPicture.children[0];
+    var pictureLikes = elementPicture.querySelector('.picture-likes');
+    var picturesComments = elementPicture.querySelector('.picture-comments');
 
-    return function (picture) {
-      var elementPicture = contentPicture.cloneNode(true);
-      var pictureImg = elementPicture.children[0];
-      var pictureLikes = elementPicture.querySelector('.picture-likes');
-      var picturesComments = elementPicture.querySelector('.picture-comments');
+    pictureImg.setAttribute('src', picture.url);
+    pictureLikes.innerText = picture.likes;
+    picturesComments.innerText = picture.comments.length;
 
-      pictureImg.setAttribute('src', picture.url);
-      pictureLikes.innerText = picture.likes;
-      picturesComments.innerText = picture.comments.length;
+    elementPicture.addEventListener('click', function (event) {
+      event.preventDefault();
 
-      elementPicture.addEventListener('click', function (event) {
-        event.preventDefault();
+      window.showGallery(picture);
+    });
 
-        window.showGallery(picture);
-      });
+    elementPicture.addEventListener('keydown', function (event) {
+      switch (event.keyCode) {
+        case ENTER_KEY :
+          window.showGallery(picture);
+      }
+    });
 
-      elementPicture.addEventListener('keydown', function (event) {
-        switch (event.keyCode) {
-          case ENTER_KEY :
-            window.showGallery(picture);
-        }
-      });
-
-      return elementPicture;
-    };
-  })();
+    return elementPicture;
+  };
 
   window.load(DATA_URL, onLoadPictures);
 
